e89fbec8b60782779467ad1c0cfba6cf0de9337b
Portinary <Portinary@proton.me>
2026-09-18 00:03:48 +0000

    Update questionablequesting.js

--- javascript/novel/src/en/questionablequesting.js
+++ javascript/novel/src/en/questionablequesting.js
@@ -7,7 +7,7 @@ const mangayomiSources = [{
   "typeSource": "single",
   "isManga": false,
   "itemType": 0,
-  "version": "1.0.0",
+  "version": "1.0.1",
   "dateFormat": "",
   "dateFormatLocale": "",
   "isNsfw": true,
