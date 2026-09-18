d27f6dacdcaf6567d65449afdb2f69a910c4de42
Portinary <Portinary@proton.me>
2026-09-18 00:56:44 +0000

    Update questionablequesting.js

--- javascript/novel/src/en/questionablequesting.js
+++ javascript/novel/src/en/questionablequesting.js
@@ -6,7 +6,7 @@ const mangayomiSources = [{
   "iconUrl": "https://forum.questionablequesting.com/favicon.ico",
   "typeSource": "single",
   "itemType": 2,
-  "version": "1.0.6",
+  "version": "1.0.7",
   "pkgPath": "",
   "notes": ""
 }];
